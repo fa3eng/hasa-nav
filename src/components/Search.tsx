1595459a3a0689ff52281247465b306ed2f51851
meakle <meakle@qq.com>
2021-02-24 22:37:24 +0800

    完成Search组件localStorage部分

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,44 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const SearchBar = styled.form`
   
 `;
 
-const Search:React.FC = () => {
-  const [value, setValue] = useState('');
-  const [engin, setEngin] = useState({
-    action: '',
-    name: ''
-  });
+const Search: React.FC = () => {
 
+  const localStorage = window.localStorage;
+  if(localStorage.getItem('enginData') === null){
+    // 确保localStorage存在
+    localStorage.setItem('enginData', JSON.stringify({
+      action: '',
+      name: ''
+    }));
+  }
+  const enginData = localStorage.getItem('enginData');
+  const [value, setValue] = useState('');
+  const [engin, setEngin] = useState(JSON.parse(enginData as string));
+
+  useEffect(() => {
+    if ((!localStorage.getItem('enginData')) || engin.action === '') {
+      const enginTemp = {
+        action: 'https://www.google.com/search',
+        name: 'q'
+      };
+      setEngin(enginTemp);
+      localStorage.setItem('enginData', JSON.stringify(enginTemp));
+      
+    } else {
+      localStorage.setItem('enginData', JSON.stringify(engin));
+    }
+  }, [engin]);
+
+  // 切换搜索引擎
+  // todo 这里以后应该要支持自定义搜索引擎
   const handleClick = () => {
-    // 对输入的字符进行处理
-    const result =  value.split(' ');
-    // 确认搜索引擎
-    if(result[0] === ':gg'){
+    const result = value.split(' ');
+    if (result[0] === ':gg') {
       setEngin({
         action: 'https://www.google.com/search',
         name: 'q'
       });
       setValue(result.slice(1).join(' '));
-    } else if(result[0] === ':bd'){
+    } else if (result[0] === ':bd') {
       setEngin({
         action: 'https://www.baidu.com/s',
         name: 'wd'
       });
       setValue(result.slice(1).join(' '));
-    }
+    }    
   };
 
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
 
   return (
     <>
-      <SearchBar action={engin.action||'https://www.google.com/search'} method="GET" target="_blank" name="searchForm">
-        <input type="text" name={engin.name || 'q'} value={value} onChange={handleChange} />
+      <SearchBar action={engin.action} method="GET" target="_blank" name="searchForm">
+        <input type="text" name={engin.name} value={value} onChange={handleChange} />
         <button type="submit" onClick={handleClick}>搜索</button>
       </SearchBar>
     </>
