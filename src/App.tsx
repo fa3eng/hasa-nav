b17ec96e6a041fd6d2f0d9fdbe128b9ec467ca20
meakle <meakle@qq.com>
2021-02-26 22:43:22 +0800

    增加页脚

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,25 @@ import styled from 'styled-components';
 import Search from './components/Search';
 import MoreSite from './components/MoreSite/MoreSite';
 import Logo from './components/Logo/Logo'
+import Footer from './components/Footer'
 
 const Layout = styled.div`
   display: flex;
   flex-flow: column;
   align-items: center;
-  /* padding-top: 280px; */
+  min-height: 95vh;
 `;
 
 const App:React.FC = () => {
   return (
-    <Layout>
-      <Logo/>
-      <Search/>
-      <MoreSite/>
-    </Layout>
+    <>
+      <Layout>
+        <Logo/>
+        <Search/>
+        <MoreSite/>
+      </Layout>
+      <Footer/>
+    </>
   )
 };
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { YuqueOutlined, GithubOutlined } from '@ant-design/icons'
+import styled from 'styled-components'
+
+const Footer = styled.footer`
+    padding: 0px 100px;
+    text-align: center;
+    font-size: 15px;
+    margin-bottom: 10px;
+    color: #bbbbbb;
+`
+
+const A = styled.a`
+    color: #bbbbbb;
+    margin: 15px;
+    :hover{
+        color: #40a9ff;
+    }
+`
+
+export default function index() {
+    return (
+        <Footer>
+            <A href="https://www.yuque.com/meakle" target="_blank"><YuqueOutlined /></A>
+            <A href="http://meakle.com/" target="_blank">@&nbsp;方阿森</A>
+            <A href="https://github.com/meakle" target="_blank"><GithubOutlined /></A>
+        </Footer>
+    )
+}
